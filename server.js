a7d1967457bc1239a0c90d8e3b4f466b6ea07005
Mridul <mit.ash.619@gmail.com>
2023-06-27 11:15:06 +0530

    redis caching basics

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/photos', async (req, res) => {
+  const albumId = req.query.albumId;
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos`,
+    {
+      params: { albumId },
+    },
+  );
+
+  res.json(data);
+});
+
+app.get('/photos/:id', async (req, res) => {
+  debugger;
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`,
+  );
+
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
